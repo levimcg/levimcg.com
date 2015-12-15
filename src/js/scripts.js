544832a43b79150cad6fafe9a7a75165d201fffa
Levi McGranahan <levimcg@gmail.com>
2015-12-15 13:01:13 -0500

    Adds feature detection for flexbox

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
+/*
+**  Nice little feature test here instead of using Modernizr
+**  https://24ways.org/2015/putting-my-patterns-through-their-paces/
+*/
 
-  // Put your scripts here!
-  
-});
+var doc = document.body || document.documentElement;
+var style = doc.style;
+
+if ( style.webkitFlexWrap == '' ||
+    style.msFlexWrap == '' ||
+    style.flexWrap == '' ) {
+  doc.className += " supports-flex";
+} else {
+    doc.className += " no-flex";
+}
+
+/*
+**  NOTE: Feature detection - if this stuff is true go wild
+**  with the JavaScript features. It means that the browser
+**  is using JavaScript and probably has good CSS3 support.
+*/
+
+/* if(document.querySelector && window.eventListener) {
+    // Enhance !
+} */
