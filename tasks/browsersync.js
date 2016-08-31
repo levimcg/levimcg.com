64d4bf0b3fca9d37613df95f3753e59896a4189d
Levi McGranahan <levimcg@gmail.com>
2016-08-31 13:57:59 -0400

    Fixes some html meta properties

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -8,11 +8,12 @@ gulp.task('browserSync', function() {
     browserSync({
         server: {
             baseDir: '_build'
-        }
+        },
+        open: false
     });
 });
 
 // Watch _build folder for changes and reload
 gulp.task('reload', ['browserSync'], function() {
-    gulp.watch('_build/**/*.*').on('change', browserSync.reload);
+    gulp.watch(['_build/**/*.html', '_build/**/*.css', '_build/**/*.js']).on('change', browserSync.reload);
 });
