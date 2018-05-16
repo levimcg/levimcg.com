0d8e8f2438b090dbde3c376f7d1401a6a9fca969
levimcg <levimcg@gmail.com>
2018-05-16 15:52:09 -0400

    Convert indentation to two spaces

--- src/js/bookshelf.js
+++ src/js/bookshelf.js
@@ -3,96 +3,96 @@
  * the browser supports our code.
  */
 if (
-    "querySelector" &&
-    "addEventListener" &&
-    "classList" &&
-    Vue.component() in window
+  "querySelector" &&
+  "addEventListener" &&
+  "classList" &&
+  Vue.component() in window
 ) {
-    // Select our library container.
-    var library = document.querySelector(".library");
-    // If our browser is capable, remove our fallback.
-    library.classList.remove("no-mustard");
+  // Select our library container.
+  var library = document.querySelector(".library")
+  // If our browser is capable, remove our fallback.
+  library.classList.remove("no-mustard")
 
-    var vm = new Vue({
-        el: "#library-app",
+  var vm = new Vue({
+    el: "#library-app",
 
-        data: {
-            title: "Libary",
-            libraryBooks: [],
-            filteredBooks: [],
-            currentTag: ""
-        },
+    data: {
+      title: "Libary",
+      libraryBooks: [],
+      filteredBooks: [],
+      currentTag: ""
+    },
 
-        computed: {
-            bookTags: function() {
-                var allTags = [];
+    computed: {
+      bookTags: function() {
+        var allTags = []
 
-                this.libraryBooks.forEach(function(book) {
-                    // allTags.push(...book.tags);
-                    book.tags.forEach(function(tag) {
-                        allTags.push(tag);
-                    });
-                });
+        this.libraryBooks.forEach(function(book) {
+          // allTags.push(...book.tags);
+          book.tags.forEach(function(tag) {
+            allTags.push(tag)
+          })
+        })
 
-                /**
-                 * This is a nice trick to return a unique array with all
-                 * duplicate items removed.
-                 *
-                 * http://mikeheavers.com/tutorials/removing_duplicates_in_an_array_using_javascript/
-                 */
-                var uniqueTags = allTags.filter(function(tag, index) {
-                    return allTags.indexOf(tag) == index;
-                });
+        /**
+         * This is a nice trick to return a unique array with all
+         * duplicate items removed.
+         *
+         * http://mikeheavers.com/tutorials/removing_duplicates_in_an_array_using_javascript/
+         */
+        var uniqueTags = allTags.filter(function(tag, index) {
+          return allTags.indexOf(tag) == index
+        })
 
-                return uniqueTags;
-            }
-        },
+        return uniqueTags
+      }
+    },
 
-        methods: {
-            getLibraryBooks: function() {
-                var req = new XMLHttpRequest();
-                req.open("GET", "../bookshelf/bookshelf.json");
+    methods: {
+      getLibraryBooks: function() {
+        var req = new XMLHttpRequest()
+        req.open("GET", "../bookshelf/bookshelf.json")
 
-                req.onload = function() {
-                    var books = JSON.parse(req.responseText);
-                    this.libraryBooks = books;
-                    this.filteredBooks = this.libraryBooks;
-                }.bind(this);
-                req.send();
-            },
+        req.onload = function() {
+          var books = JSON.parse(req.responseText)
+          this.libraryBooks = books
+          this.filteredBooks = this.libraryBooks
+        }.bind(this)
+        req.send()
+      },
 
-            handleFilter: function(e) {
-                var currentFilter = e.target.getAttribute("data-filter");
-                this.currentTag = currentFilter;
+      handleFilter: function(e) {
+        var currentFilter = e.target.getAttribute("data-filter")
+        this.currentTag = currentFilter
 
-                var filteredBookArr = this.libraryBooks.filter(function(book) {
-                    return book.tags.indexOf(currentFilter) > -1;
-                });
+        var filteredBookArr = this.libraryBooks.filter(function(book) {
+          return book.tags.indexOf(currentFilter) > -1
+        })
 
-                this.filteredBooks = filteredBookArr;
-            },
+        this.filteredBooks = filteredBookArr
+      },
 
-            resetFilter: function() {
-                this.filteredBooks = this.libraryBooks;
-                this.currentTag = "all";
-            }
-        },
+      resetFilter: function() {
+        this.filteredBooks = this.libraryBooks
+        this.currentTag = "all"
+      }
+    },
 
-        filters: {
-            /**
-             * Got this one here:
-             * https://vuejs.org/v2/guide/filters.html
-             */
-            capitalize: function(value) {
-                if (!value) return "";
-                value = value.toString();
-                return value.charAt(0).toUpperCase() + value.slice(1);
-            }
-        },
+    filters: {
+      /**
+       * Got this one here:
+       * https://vuejs.org/v2/guide/filters.html
+       */
+      capitalize: function(value) {
+        if (!value) return ""
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      }
+    },
 
-        mounted: function() {
-            this.getLibraryBooks();
-            this.filteredBooks = this.libraryBooks;
-        }
-    });
+    mounted: function() {
+      this.getLibraryBooks()
+      this.filteredBooks = this.libraryBooks
+    }
+  })
 }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,11 @@
 /*
-**  Creates a dynamic copyright date and inserts it
-**  into the page.
-*/
+ *  Creates a dynamic copyright date and inserts it
+ *  into the page.
+ */
 var elCopyRight = document.getElementById('mcg-copy-year');
 var currentDate = new Date().getFullYear();
 
-if(elCopyRight) {
-    elCopyRight.innerText = currentDate;
+if (elCopyRight) {
+  elCopyRight.innerText = currentDate;
 }
 
