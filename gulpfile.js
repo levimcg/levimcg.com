846b224fcf632d9a39e91022a87aaa25514b49f3
levimcg <levimcg@gmail.com>
2018-05-15 21:40:33 -0400

    Reworked gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var requireDir = require('require-dir');
-
-// Include everything in the "tasks" folder
-requireDir('./tasks');
-
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
 const cp = require('child_process');
 const gutil = require('gulp-util');
+const sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync');
 
 gulp.task('compile', function(callback) {
   cp.exec('jekyll build', function(err, stdout, stderr) {
@@ -29,19 +29,59 @@ gulp.task('compile:watch', function() {
   jekyll.stderr.on('data', jekyllLogger);
 });
 
-/*
-**  Frequent Tasks
-*/
-
-// Build for production
-gulp.task('build', function(cb) {
-  runSequence('jekyllProd', 'sass', 'prefix', 'cssProd', 'jsProd', cb);
+gulp.task('sass', function () {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('./_build/css'))
+    .pipe(browserSync.stream());
 });
 
-// Build and deploy for production
-gulp.task('ship', function(cb) {
-    runSequence('build', 'deploy');
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
 });
 
-// Default dev task
-gulp.task('default', ['jekyllWatch', 'sass', 'sass:watch', 'reload']);
+gulp.task('css:minify', function () {
+  return gulp.src('_build/css/style.css')
+    .pipe(cssnano())
+    .pipe(gulp.dest('_build/css/'));
+});
+
+gulp.task('css:prefix', function () {
+  return gulp.src('_build/css/style.css')
+    .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+    .pipe(gulp.dest('_build/css/'));
+});
+
+/**
+ *  Browsersync Tasks
+ */
+gulp.task('serve', function () {
+  browserSync({
+    server: {
+      baseDir: '_build'
+    }
+  });
+
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('_build/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('build', function(done) {
+  runSequence('compile', 'sass', 'css:prefix', 'css:minify', done);
+});
+
+gulp.task('deploy', function () {
+  cp.exec('aws s3 sync _build/ s3://levimcg.com', function (err, stdout, stderr) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+});
+
+gulp.task('ship', function(done) {
+  runSequence('build', 'deploy');
+});
+
+gulp.task('default', ['compile:watch', 'serve']);
--- tasks/postcss.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-
-gulp.task('prefix', function() {
-    return gulp.src('_build/css/*.css')
-        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-        .pipe(gulp.dest('_build/css'));
-});
