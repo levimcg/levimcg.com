54a0715cdb3b2c276d557811eeda144b2ce9e13c
levimcg <levimcg@gmail.com>
2018-05-15 20:29:39 -0400

    Adds child process for Jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,37 @@ var requireDir = require('require-dir');
 // Include everything in the "tasks" folder
 requireDir('./tasks');
 
+const cp = require('child_process');
+const gutil = require('gulp-util');
+
+gulp.task('compile', function(callback) {
+  cp.exec('jekyll build', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    callback(err);
+  });
+});
+
+gulp.task('compile:watch', function() {
+  const jekyll = cp.spawn('jekyll', ['build', '--watch']);
+
+  const jekyllLogger = function (buffer) {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
 /*
 **  Frequent Tasks
 */
 
 // Build for production
 gulp.task('build', function(cb) {
-    runSequence('jekyllProd', 'sass', 'prefix', 'cssProd', 'jsProd', cb);
+  runSequence('jekyllProd', 'sass', 'prefix', 'cssProd', 'jsProd', cb);
 });
 
 // Build and deploy for production
