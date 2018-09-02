6af50fe9d546944dff802adce0b1d205769f1ac5
levimcg <levimcg@gmail.com>
2018-09-01 22:04:41 -0400

    Adds 11ty plugin for syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const CleanCSS = require('clean-css');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function(eleventyConfig) {
+  // Syntax highlighting
+  eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addFilter(
     'cssmin',
     code => new CleanCSS({}).minify(code).styles
