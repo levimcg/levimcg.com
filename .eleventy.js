f0a64aad632fe7a32425a12699e34f9ca1d35bdf
levimcg <levimcg@gmail.com>
2019-01-02 14:53:37 -0500

    Adds new post and favicon

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy('src/favicon.png');
 
   return {
     dir: {
