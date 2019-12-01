93ec7b253ec36c1fcb81bf35adaa93cd8c78987f
Levi <levimcg@gmail.com>
2019-12-01 14:39:36 -0500

    Replace lazysizes with IntersectionObserver

--- src/_includes/js/site.js
+++ src/_includes/js/site.js
@@ -27,3 +27,32 @@ const theme = new Themur({
   themeClass: 'mcg-dark-theme',
   useLocalStorage: true
 });
+
+// Lazy load images
+const images = Array.prototype.slice.call(
+  document.querySelectorAll('img')
+);
+
+function handleIntersection(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0 && !entry.target.hasAttribute('src')) {
+      const imageSrc = entry.target.dataset.src;
+      entry.target.setAttribute('src', imageSrc);
+      entry.target.setAttribute('class', 'lazyloaded');
+    }
+  });
+}
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver(handleIntersection, {
+    threshold: 0.1
+  });
+
+  images.forEach(image => {
+    observer.observe(image);
+  });
+} else {
+  images.forEach(image => {
+    image.setAttribute('src', image.dataset.src);
+  })
+}
