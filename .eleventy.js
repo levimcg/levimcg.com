3e47278d5fbdf17f01d61c3bafd823bfe28bd1d1
Levi McGranahan <lmcgrana@iu.edu>
2022-02-11 18:45:43 -0500

    Added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -61,6 +61,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/favicon.png');
+  eleventyConfig.addPassthroughCopy('src/icon.svg');
 
   // BrowserSync settings
   eleventyConfig.setBrowserSyncConfig({
