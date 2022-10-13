65ca9c51381bcd6e967a75bb80e53ccfb00ca40f
Levi McGranahan <lmcgrana@iu.edu>
2022-10-13 13:37:24 -0400

    Grouped posts by year

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
-const htmlMinifier = require('html-minifier');
+const htmlMinifier = require('html-minifier')
+const _ = require('lodash')
 
 // 11ty plugins
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const markdownIt = require('markdown-it');
-const markdownItAnchor = require('markdown-it-anchor');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
 
 // Import filters
 const dateFormatted = require('./src/filters/dateFormatted');
@@ -43,9 +44,11 @@ module.exports = function(eleventyConfig) {
   // Filters
   eleventyConfig.addFilter('dateFormatted', dateFormatted);
 
+  // Collections
+
   // Creates a list of all tags
   function filterTagList(tags) {
-    return (tags || []).filter(tag => ["all", "nav", "post", "note"].indexOf(tag) === -1);
+    return (tags || []).filter(tag => ["all", "nav", "post", "notes", "projects"].indexOf(tag) === -1);
   }
 
   eleventyConfig.addCollection("tagList", function(collection) {
@@ -55,7 +58,19 @@ module.exports = function(eleventyConfig) {
     });
 
     return filterTagList([...tagSet]);
-  });
+  })
+
+  /**
+   * This post was super helpful in figuring out how to sort this way
+   * https://darekkay.com/blog/eleventy-group-posts-by-year/
+   */
+  eleventyConfig.addCollection('postsByYear', collection => {
+    return _.chain(collection.getFilteredByTag('post'))
+      .groupBy(post => post.date.getFullYear())
+      .toPairs()
+      .reverse()
+      .value()
+  })
 
   // Files to watch and copy on change
   eleventyConfig.addPassthroughCopy('src/fonts');
